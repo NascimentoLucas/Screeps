164b3b55b02fe5a4ff891c32f1ba8c3ef6e74833
NascimentoLucas <nascimentol@outlook.com>
2019-05-29 05:57:57 -0300

    implementing repair

--- tool.builder.js
+++ tool.builder.js
@@ -5,17 +5,54 @@ var main = {
 
     /** @param {Creep} creep **/
     builder: function(creep) {
-		var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 		if(creep.carry.energy > 0){
-			if(targets.length > 0) {
+			var targets = sFinderStructureRepair(creep, null)
+			if(targets == null){
+				if(creep.repair(targets) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(targets, {visualizePathStyle: {stroke: '#ffffff'}});
+				}
+				return true;
+			}			
+			else {
+				targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+				if(targets.length > 0) {
 				if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
 				}
 				return true;
 			}
+			}
+				
 		}
 		return false;
 	}
 };
+
+function sFinderStructureRepair(creep, type) {
+	//structure.structureType == type & 
+    var s = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+            return ((structure.hits < structure.hitsMax));
+        }
+    });
+    if (s.length > 0)
+    {
+        var min = 10000000;
+        var target;
+        for (const i in s)
+        {
+            if (s[i].hits < min)
+            {
+                min = s[i].hits;
+                target = s[i];
+            }
+        }
+        return target;
+    }
+    else
+    {
+        return null;
+    }
+}
 
 module.exports = main;
